44520f7087dda5464f3d2a5e1886e7b192219ec3
Sumit <sumitvivo0040@gmail.com>
2026-03-10 23:18:53 +0530

    Fix space

--- for_loops.py
+++ for_loops.py
@@ -33,7 +33,6 @@ for i in range (1,n+1):
 print(f"The factorial of {n} is {product}")
 
 # Write a program to find whether a given number is prime or not ..
-
 n = int(input("Enter the number: "))
 for i in range (2,n):
     if (n%i) == 0:
