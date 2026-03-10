ef5c55cf1e383614bb0f7ec796c1120f38b30c0d
Sumit <sumitvivo0040@gmail.com>
2026-03-10 23:24:50 +0530

    Add comments in code

--- rules_variable.py
+++ rules_variable.py
@@ -1,12 +1,16 @@
 # some rule's in variable's
 # to check a valid or invalid variable's 
 
+# Value of a is..
 a = 5  # valid 
 
+# Value of b is ..
 sumit = 10  # valid
 
+# Value of rani is ..
 rani = 15  # valid
 
+# value of _rani is..
 _rani = 45  # valid
 
 # @puja = 54 is a invalid due to @ symbol
