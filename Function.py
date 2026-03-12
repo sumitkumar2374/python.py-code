2bf848cf672a8a1368ac2a730119505f0669cc68
Sumit <sumitvivo0040@gmail.com>
2026-03-12 19:51:00 +0530

    Add function & recursion

---
+++ Function.py
@@ -0,0 +1,68 @@
+""" Functions:
+            A function is a group of statement performing a specific task ..
+            When a program gets bigger in size and its complexity grows, it gets difficult for a program to keep track on which piece of code is doing what! ..
+            A function can be reused by the programmer in a given program any number of .. """
+            
+# Syntax:
+def func1():
+    print("hello")  # This Function can be call any number of times, anywhere in the program ..
+
+            
+""" Function call:
+                Whenever we want to call a function, we put the name of the function followed by parentheses as follows .. """
+                
+# Syntax:
+func1()  # This is called function call ..
+
+""" Function Defination:
+                The part containing the exact set of instructions which are executed during the function call.. """
+                
+# Q. Write a program to greet a user with "Good Day" using functions ..
+def goodDay():
+    print("Good Day")
+goodDay()
+
+
+# Type of Functions ..
+""" Type of function in python: 
+    These are two type of function in python ..
+    
+    1. Built in functions (EX: print(), range(), len() etc..)
+    2. user defined functions (EX: func1(), printHello(), greet() etc ..) """
+
+ 
+ # Function with arguments ..   
+""" Function with arguments:
+                        A function can accept some value it can work with we can put these valuese in the parenthese..
+                        A function can also return value as show below.. """
+# EXAMPLE:-                    
+def goodDay(name,ending):
+    print("Good day" + name)
+    print(ending)
+    
+goodDay("Harry","Thank you")
+goodDay("Rohan","Thank You")
+goodDay("Divya","Thank You")
+
+
+# Default parameter value ..
+""" Default Parameter Value:-
+    
+    1. We can have a value as default as default argument in a function ..
+    2. If we specify name = "stranger" in the line containing def this value is used when no argument is passsed."""
+# EXAMPLE 1.  
+def greet (name = "stranger"):
+    # function body
+    greet()
+    greet("Harry")
+    
+# EXAMPLE 2.
+def goodDay (name,ending = "Thank You"):
+    print(f"Good Day,{name}")
+    print(ending)
+    
+goodDay("Harry","Thanks")
+goodDay("Rohan")          # isme by default parameter add ho jayega!
+
+print("Here are the full theory and code for functions in python!")
+    
---
+++ recursion.py
@@ -0,0 +1,29 @@
+""" Recursion: 
+            Recursion is a function which call itself..
+            It's used to directly use a mathematical as function.. 
+            
+            EXAMPLE:
+                factorial(n) = n x factorial (n-1)
+                This function can be defined as follows: """
+                
+# Print Factorial using recursion ..
+n = int(input("Enter a number: "))
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n*factorial(n-1)
+print(f"The Factorial of this number is: {factorial(n)}")
+
+"""     This work as follows:
+            
+            Factorial(5)
+            5 x Factorial(4)
+            5 x 4 x Factorial(3)
+            5 x 4 x 3 Factorial(2)
+            5 x 4 x 3 x 2 Factorial(1)
+            5 x 4 x 3 x 2 x 1   -----> 120     """
+            
+            
+
+
